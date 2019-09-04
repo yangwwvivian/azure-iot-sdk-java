9d2c2006ec59bbfd5d743f44a840975725210096
David Lu <davilu@microsoft.com>
2019-09-04 11:34:45 -0700

    feat(iot-dev, e2e, sample): Modified api version and removed property key validation

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/DeviceTwin/Property.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/DeviceTwin/Property.java
@@ -15,21 +15,6 @@ public class Property
 
     public Property(String key, Object value)
     {
-        if (key == null || key.isEmpty())
-        {
-
-            /*
-            **Codes_SRS_Property_25_002: [**If the key is null or empty, the constructor shall throw an IllegalArgumentException.**]
-             */
-            throw new IllegalArgumentException("Key cannot be null or empty");
-        }
-        if (key.contains(" ") || key.contains("$") || key.contains("."))
-        {
-            /*
-            **Codes_SRS_Property_25_006: [**If the key contains illegal unicode control characters i.e ' ', '.', '$', the constructor shall throw an IllegalArgumentException.**]**
-             */
-            throw new IllegalArgumentException("Key cannot contain illegal unicode control characters '.', '$', ' '");
-        }
         /*
         **Codes_SRS_Property_25_001: [**The constructor shall save the key and value representing this property.**]**
          */
